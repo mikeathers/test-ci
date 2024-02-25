e57d940f9427a6aff8aec98d9ca1a026a8375c9f
Mike Atherton <mikeatherton06@gmail.com>
2024-02-25 16:23:30 +0000

    6

--- destroy-stack-on-merge.js
+++ destroy-stack-on-merge.js
@@ -1,20 +1,9 @@
-async function main() {
-  if (!process.env.BRANCH_NAME) throw Error('A BRANCH_NAME must be provided...')
-  if (!process.env.DOMAIN_NAME) throw Error('A DOMAIN_NAME must be provided...')
-  if (!process.env.SUBDOMAIN_NAME) throw Error('A SUBDOMAIN_NAME must be provided...')
-  if (!process.env.STACK_URL) throw Error('A STACK_URL must be provided...')
-  if (!process.env.STACK_NAME) throw Error('A STACK_NAME must be provided...')
-  if (!process.env.GITHUB_REPOSITORY)
-    throw Error('A GITHUB_REPOSITORY must be provided...')
+const process = require('process');
 
-  console.log({
-    branch: process.env.BRANCH_NAME,
-    domainName: process.env.DOMAIN_NAME,
-    subDomain: process.env.SUBDOMAIN_NAME,
-    githubRepo: process.env.GITHUB_REPOSITORY,
-    stackUrl: process.env.STACK_URL,
-    stackName: process.env.STACK_NAME,
-  })
+
+async function main() {
+  console.log(process.argv[2]);
+
 }
 
 main()
